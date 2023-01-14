ed421259c6554719ed37d9692d9295601420763c
solonovamax <solonovamax@12oclockpoint.com>
2023-01-14 14:10:55 -0500

    Fix player collisions

--- Assets/Chest.cs
+++ Assets/Chest.cs
@@ -2,32 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour, Interactable {
+public class Chest : MonoBehaviour, Interactable {
     // Start is called before the first frame update
     public  SpriteRenderer sr;
     public  Sprite         NewSprite;
     public  Collider2D     coll;
-    private bool           change;
-
-
-    private void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log(other.gameObject.tag);
-
-        if (other.gameObject.CompareTag("Interact")) {
-            Debug.Log("working");
-
-            gameObject.GetComponentInChildren<SpriteRenderer>().sprite = NewSprite;
-        }
-    }
+    private bool           canInteract;
 
     private void interact(Collision2D player) {
         Debug.Log("Interaction");
     }
+
     /*  private void OnCollisionExit2D(Collision2D other)
       {
           tag = "Player";
       }*/
     public void playerInteraction(Player player) {
+        if (!canInteract)
+            return;
+
+        if (!Input.GetMouseButtonDown(0))
+            return;
+
         Debug.Log("Interaction");
 
         // throw new System.NotImplementedException();
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,9 +1,9 @@
-using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour {
-    public float playerSpeed = 5f;
+    private static readonly int   animIsMovingParameter    = Animator.StringToHash("IsMoving");
+    private static readonly int   animIsAttackingParameter = Animator.StringToHash("IsAttacking");
+    public                  float playerSpeed              = 5f;
 
     public Rigidbody2D    rb;
     public Animator       anim;
@@ -12,10 +12,6 @@ public class Player : MonoBehaviour {
     public  BoxCollider2D bc;
     private float         _hInput;
     private float         _vInput;
-
-    // private static readonly int   animSpeedParameter    = Animator.StringToHash("Speed");
-    private static readonly int animIsMovingParameter    = Animator.StringToHash("IsMoving");
-    private static readonly int animIsAttackingParameter = Animator.StringToHash("IsAttacking");
 
     /// <summary>
     ///     Called once per frame
@@ -28,27 +24,9 @@ public class Player : MonoBehaviour {
         anim.SetBool(animIsAttackingParameter, Input.GetMouseButtonDown(0));
     }
 
-    private void OnCollisionStay2D(Collision2D other) {
-        if (other.gameObject.CompareTag("Interactable")) {
-            var interactable = other.gameObject.GetComponent<Interactable>();
-
-            interactable?.playerInteraction(this);
-            // other.gameObject.BroadcastMessage("interact", this);
-        }
-    }
-
-    // private void OnCollisionEnter2D(Collision2D other) {
-    //     Debug.Log(other.gameObject.tag);
-    //
-    //     if (other.gameObject.CompareTag("Interact")) {
-    //         Debug.Log("working");
-    //
-    //         gameObject.GetComponentInChildren<SpriteRenderer>().sprite = NewSprite;
-    //     }
-    // }
-
     private void FixedUpdate() {
         transform.position += new Vector3(_hInput * playerSpeed * Time.deltaTime, _vInput * playerSpeed * Time.deltaTime, 0);
+
         if (_hInput > 0.01 || _vInput > 0.01)
             sr.flipX = false;
         else if (_hInput < 0.01 || _vInput < 0.01)
@@ -61,5 +39,11 @@ public class Player : MonoBehaviour {
         //     sr.flipX = true;
         // else
         //     sr.flipX = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        var interactable = other.gameObject.GetComponent<Interactable>();
+
+        interactable?.playerInteraction(this);
     }
 }
